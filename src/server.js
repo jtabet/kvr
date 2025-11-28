36cfac842717c8a333ed9861bd868c7f3dcc0030
Jérémie Tabet <dev@jeremietabet.com>
2025-11-28 23:15:10 +0100

    Raw body

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-app.use(express.json());
+app.use(bodyParser.text({type: '*/*'}));
 
 // In-memory KV store
 const store = {};
